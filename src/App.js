c627364bd7d2ee5ed4f51881872a74e1188a6493
Dhruv Garg <dhruv.g4445@gmail.com>
2024-07-13 09:51:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-   <h1>Hello ReactJS</h1>
-  );
+//now write the first component
+//arrow function
+const App = () =>{
+  return(
+    <h1>Hello ReactJS!!</h1>
+  )
 }
-
 export default App;
